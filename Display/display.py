b6ccac7663625b92a136317e6505ee2d0003b16e
Samirou19 <samir.otmani@live.fr>
2020-04-23 02:51:40 +0200

    message

--- Display/display.py
+++ Display/display.py
@@ -1,24 +1,19 @@
-from AjoutsFilm.Presenter.PresenterDisplay import Presenter
+class display():
 
-class display() :
+    def __init__(self):
+        self.Nom = ""
 
-	def __init__(self) : 
-		# self.continuer = "o"
-		# self.ListeNom = []
-		self.Nom = ""
+    def DemandeUser(self):
+        self.user = input("Rentre ton user")
 
-	def DemandeUser(self) : 
-		   self.user = raw_input("Rentre ton user")
+    def Continuer(self):
+        self.continuer = input("Continuer?")
 
-	def Continuer(self) : 
-		self.continuer = raw_input("Continuer?")
+    def giveUser(self):
+        return self.ListeNom
 
-	def giveUser (self) : 
-		return self.ListeNom
+    def DemanderFilm(self):
+        input("Rentre ton Film")
 
-
-	def DemanderFilm(): 
-		raw_input("Rentre ton Film")
-
-	def RentreTanote():
-		pass
+    def RentreTanote(self):
+        pass
